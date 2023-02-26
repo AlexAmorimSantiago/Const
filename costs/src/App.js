6d517a300381321f9584c7fa268d7a5503668a08
ALEX.A.SANTIAGO <alexsantiago87@yahoo.com.br>
2023-02-26 10:51:49 -0300

    config o projeto

---
+++ costs/src/App.js
@@ -0,0 +1,12 @@
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+
+function App() {
+  return (
+    <div className="App">
+      <p>Costs</p>
+    </div>
+  );
+}
+
+export default App;
