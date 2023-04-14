2de2f6e93358cc1bb1e21bd6233f0708d9f30d1b
ALEX.A.SANTIAGO <alexsantiago87@yahoo.com.br>
2023-04-13 22:28:56 -0300

    Pagina Home concluida

--- costs/src/componentes/pages/Home.js
+++ costs/src/componentes/pages/Home.js
@@ -1,4 +1,16 @@
+import style from './Home.module.css'
+import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton'
+
 function Home(){
-    return <h1>Home</h1>
+    return(
+        <section className={style.home_container}>
+            <h1>Bem - vindo ao <span>costs</span></h1>
+            <p>Comece a gerenciar o seus projetos agora mesmo!</p>
+            <LinkButton to="/newproject" text="Criar projeto" />
+            <img src={savings} alt="Costs"/>
+
+        </section>
+    )
 }
 export default Home
